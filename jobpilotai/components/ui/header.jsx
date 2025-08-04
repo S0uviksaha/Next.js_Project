4be251fc5a04f64506113074b08f7d56d0ab3681
SOUVIK SAHA <souvik21102001@gmail.com>
2025-08-04 09:18:35 +0530

    header updated

--- jobpilotai/components/ui/header.jsx
+++ jobpilotai/components/ui/header.jsx
@@ -1,17 +1,36 @@
 // components/ui/header.js (or .tsx)
 import { SignedOut, SignInButton, SignedIn, UserButton } from '@clerk/nextjs';
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import Button from '/ui/button';
 
 function Header() {
   return (
-    <div className="flex justify-end p-4">
+    <header className="flex justify-end p-4">
+      <nav>
+        <Link href='/'>
+          <Image
+          src ="/logo3.png" alt ="JobPilot AI Logo" width={50} height={50} className="h-20 py-1 w-auto object-contain"
+          />
+        </Link>
+
+      <div>
+        <SignedIn>
+          <Link>
+            <Button></Button>
+          </Link>
+        </SignedIn>
+      </div>  
+      </nav>
+
+
       <SignedOut>
         <SignInButton />
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
-    </div>
+    </header>
   );
 }
 
